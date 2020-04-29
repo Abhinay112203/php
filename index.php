5ca9ae206f7cfae6b8c9f73878cb9fc19325aa79
Abhinay-CQS <abhinay@cqs.in>
2020-04-30 00:37:40 +0530

    Initial Commit

---
+++ index.php
@@ -0,0 +1,376 @@
+
+<html>
+  <head>
+    <title>AskUsBuddy</title>
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, user-scalable=no"
+    />
+    <link rel="stylesheet" href="assets/css/main.css" />
+    <link rel="stylesheet" href="assets/css/logo.css" />
+    <noscript
+      ><link rel="stylesheet" href="assets/css/noscript.css"
+    /></noscript>
+  </head>
+  <body class="is-preload">
+    <!-- Wrapper -->
+    <div id="wrapper">
+      <!-- Header -->
+      <header id="header" class="alt">
+        <span class="logo">
+          <img src="assets/images/Aub-logo.png" alt="" />
+        </span>
+        <span>
+          <h2 style="font-weight: 500; padding-bottom: 0; margin-bottom: 0;">
+            Ask Us Buddy
+          </h2>
+        </span>
+        <p
+          style="
+            border: 2px solid;
+            border-left: 0;
+            border-right: 0;
+            display: inline-block;
+            margin-top: 0;
+            padding-top: 0;
+            padding-left: 40px;
+            padding-right: 40px;
+          "
+        >
+          One click for all your needs
+        </p>
+      </header>
+
+      <!-- Nav -->
+      <nav id="nav">
+        <ul>
+          <li><a href="#intro" class="active">About Us</a></li>
+          <li><a href="#mission">Ask Us Buddy Mission</a></li>
+          <li><a href="#first">Our Services</a></li>
+          <li><a href="#second">Corporate Information</a></li>
+          <li><a href="#third">Key Highlights</a></li>
+          <li><a href="#cta">Contact Us</a></li>
+        </ul>
+      </nav>
+
+      <!-- Main -->
+      <div id="main">
+        <!-- Introduction -->
+        <section id="intro" class="main">
+          <div class="spotlight">
+            <div class="content">
+              <header class="major">
+                <h2>About Us</h2>
+              </header>
+              <p>
+                Ask Us Buddy Home Solutions is Local Search engine that provides
+                local search related services to users through multiple
+                platforms such as website, mobile website, Apps (Android, iOS,
+                Windows).Ask Us Buddy has also initiated ‘Search Plus’ services
+                for its users. These services aim at making several day-to-day
+                tasks conveniently actionable and accessible to users through
+                one App. By doing so, it has transitioned from being purely a
+                provider of local search and related information to being an
+                enabler of such transactions. Ask Us Buddy has also recently
+                launched an end-to-end business management solution for SMEs,
+                through which it intends to transition thousands of SMEs to
+                efficiently run their business online and have adequate online
+                presence via their own website and mobile site. The organisation
+                also aims to make communication between users and businesses.
+              </p>
+              <!-- <ul class="actions">
+                <li><a href="generic.html" class="button">Learn More</a></li>
+              </ul> -->
+            </div>
+            <!-- <span class="image"><img src="images/pic01.jpg" alt="" /></span> -->
+          </div>
+        </section>
+        <!-- mission -->
+        <section id="mission" class="main">
+          <div class="spotlight">
+            <div class="content">
+              <header class="major">
+                <h2>Ask Us Buddy Mission</h2>
+              </header>
+              <p>
+                To build platform for customer and merchants to communicate with
+                each other comfortably. Communication regarding customer’s
+                problems, service, query, and merchant’s service, goods, etc are
+                made easy. The radius of business should be increased.
+                Opportunities to the unemployed should be increased.
+              </p>
+              <!-- <ul class="actions">
+                <li><a href="generic.html" class="button">Learn More</a></li>
+              </ul> -->
+            </div>
+            <!-- <span class="image"><img src="images/pic01.jpg" alt="" /></span> -->
+          </div>
+        </section>
+        <!-- Services Section -->
+        <section id="first" class="main special">
+          <header class="major">
+            <h2>Our Services</h2>
+          </header>
+          <ul class="features">
+            <li>
+              <img class="logoX" src="assets/images/grocery.png" alt="" />
+              <h3>Grocery</h3>
+              <p>
+                We provide fresh delivery of groceries from nearby merchants at
+                great deals.
+              </p>
+            </li>
+            <li>
+              <img class="logoX" src="assets/images/Marketfresh.png" alt="" />
+              <h3>Market fresh</h3>
+              <p>
+                Vegetables and meat delivered fresh at your door step
+              </p>
+            </li>
+            <li>
+              <img
+                style="position: relative; left: 12px; z-index: 4;"
+                class="logoX"
+                src="assets/images/electrician.png"
+                alt=""
+              />
+              <img
+                style="position: relative; right: 12px; z-index: 20;"
+                class="logoX"
+                src="assets/images/Plumber.png"
+                alt=""
+              />
+              <h3>Home Services</h3>
+              <p>
+                Electrical and Plumbing services for your home at your door step
+              </p>
+            </li>
+            <li>
+              <img class="logoX" src="assets/images/Assign a work.png" alt="" />
+              <h3>Assign a work</h3>
+              <p>
+                Assign and get your works done (bring or send something)
+              </p>
+            </li>
+          </ul>
+        </section>
+
+        <!-- Second Section -->
+        <section id="second" class="main special">
+          <header class="major">
+            <h2>Corporate Information</h2>
+            <p>
+              The official website www.askusbuddy.com was launched on April 30,
+              2020.<br />
+            </p>
+            <p style="text-align: justify;">
+              Ask Us Buddy's search services are available to users across
+              multiple platforms such as website, mobile website, Apps, voice
+              and text (SMS). Ask Us Buddy's search services bridge the gap
+              between users and businesses by helping users find relevant
+              providers of products and services effortlessly, while helping
+              businesses listed in Ask Us Buddy's database to market their
+              offerings.
+            </p>
+          </header>
+        </section>
+        <!-- Key Highlights -->
+        <section id="third" class="main special">
+          <header class="major">
+            <h2>Key Highlights</h2>
+          </header>
+          <ul class="features arm">
+            <li>
+              <h3>
+                <strong
+                  ><span class="underLine">BEST QUALITY SERVICES</span></strong
+                >
+              </h3>
+              <p>
+                AskUsBuddy ensures to get the best merchants and deals
+                accordingly for getting works done at the customers door step.
+              </p>
+            </li>
+            <li>
+              <h3>
+                <strong><span class="underLine2">LOWEST PRICES</span></strong>
+              </h3>
+              <p>
+                We ensure to provide equal or lowest prices to our customers by
+                delivering the products the wish to their door.
+              </p>
+            </li>
+            <li>
+              <h3>
+                <strong><span class="underLine">EASY RETURNS</span></strong>
+              </h3>
+              <p>
+                Askusbuddy provides hassle free returns on the products
+                delivered by our partners
+              </p>
+            </li>
+            <li>
+              <h3>
+                <strong
+                  ><span class="underLine2">BEST CUSTOMER SUPPORT</span></strong
+                >
+              </h3>
+              <p>
+                You can contact our customer support team to resolve any query
+                about placing an order, products, shipping, refund, commission
+                payout status, or anything related to your reselling business.
+              </p>
+            </li>
+            <li>
+              <h3>
+                <strong
+                  ><span class="underLine"
+                    >HASSLE FREE LOGISTICS SUPPORT</span
+                  ></strong
+                >
+              </h3>
+              <p>
+                We take care of getting products from supplier/merchant and
+                deliver them to customer.
+              </p>
+            </li>
+            <li>
+              <h3>
+                <strong
+                  ><span class="underLine2"
+                    >CASH ON DELIVERY (COD)</span
+                  ></strong
+                >
+              </h3>
+              <p>
+                Build trust and increase sales by offering cash on delivery as a
+                payment option to customer.
+              </p>
+            </li>
+            <li>
+              <h3>
+                <strong><span class="underLine">IN TIME SERVICES</span></strong>
+              </h3>
+              <p>
+                Services will be provided in time as the app is going to connect
+                local merchants according to the needs of customers.
+              </p>
+            </li>
+            <li>
+              <h3>
+                <strong
+                  ><span class="underLine2">MONEY PROTECTION</span></strong
+                >
+              </h3>
+              <p>
+                Your money is safe with us. The suppliers get paid only after
+                the provision of the service to the customer.
+              </p>
+            </li>
+            <li>
+              <h3>
+                <strong><span class="underLine">SAFETY FIRST</span></strong>
+              </h3>
+              <p>
+                Customers safety is taken into consideration. The profile of all
+                the service providers is verified prior to the services they
+                provide.
+              </p>
+            </li>
+          </ul>
+        </section>
+        <!-- Get Started -->
+        <section id="cta" class="main special">
+          <header class="major">
+            <h2>Get Registered</h2>
+            <p>
+              Register now for exciting launch offers
+            </p>
+          </header>
+          <footer class="major">
+            <div class="inputForm">
+              <div>
+                <input type="text" class="form-control" />
+              </div>
+              <div
+                style="
+                  display: flex;
+                  justify-content: flex-start;
+                  align-items: flex-start;
+                  margin: 10px;
+                "
+              >
+                <button
+                  type="button"
+                  class="warning"
+                  style="background-color: #f00408; padding-bottom: 10px;"
+                >
+                  <span style="color: white; font-weight: 800;">Register</span>
+                </button>
+              </div>
+            </div>
+          </footer>
+        </section>
+      </div>
+
+      <!-- Footer -->
+      <footer id="footer">
+        <section>
+          <h2>Contact us</h2>
+          <p>
+            Connect to us through and let our progress excite you
+          </p>
+        </section>
+        <section>
+          <!-- <h2>Etiam feugiat</h2>
+          <dl class="alt">
+            <dt>Address</dt>
+            <dd>1234 Somewhere Road &bull; Nashville, TN 00000 &bull; USA</dd>
+            <dt>Phone</dt>
+            <dd>(000) 000-0000 x 0000</dd>
+            <dt>Email</dt>
+            <dd><a href="#">information@untitled.tld</a></dd>
+          </dl> -->
+          <ul class="icons">
+            <li>
+              <a href="#" class="icon brands fa-twitter alt"
+                ><span class="label">Twitter</span></a
+              >
+            </li>
+            <li>
+              <a href="#" class="icon brands fa-facebook-f alt"
+                ><span class="label">Facebook</span></a
+              >
+            </li>
+            <li>
+              <a href="#" class="icon brands fa-instagram alt"
+                ><span class="label">Instagram</span></a
+              >
+            </li>
+          </ul>
+        </section>
+      </footer>
+    </div>
+    <?php
+    echo "Hello World";
+    ?>
+
+    <!-- Scripts -->
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/jquery.scrollex.min.js"></script>
+    <script src="assets/js/jquery.scrolly.min.js"></script>
+    <script src="assets/js/browser.min.js"></script>
+    <script src="assets/js/breakpoints.min.js"></script>
+    <script src="assets/js/util.js"></script>
+    <script src="assets/js/main.js"></script>
+    <script src="/__/firebase/7.14.2/firebase-app.js"></script>
+
+    <!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+    <script src="/__/firebase/7.14.2/firebase-analytics.js"></script>
+
+    <!-- Initialize Firebase -->
+    <script src="/__/firebase/init.js"></script>
+  </body>
+</html>
